89a2026fcbda9e3315f94f7e7f8e2eb6926e7c2e
Elle Wilson <eleanorwilsonn@gmail.com>
2018-01-24 20:11:46 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -5,3 +5,43 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+def won?(board)
+WIN_COMBINATIONS.find do |win_combo|
+  board[win_combo[0]] == board[win_combo[1]] && board[win_combo[0]] == board[win_combo[2]] && position_taken?(board, win_combo[1])
+end
+end
+
+def full?(board)
+  board.none? do |i|
+    i == " " || i.nil?
+  end
+end
+
+def draw?(board)
+  won?(board) == nil && full?(board) == true
+end
+
+def over?(board)
+  if draw?(board) == true
+    true
+  elsif won?(board) != nil && full?(board) == true
+    true
+  elsif won?(board) != nil && full?(board) == false
+    true
+  else
+    false
+  end
+end
+
+def winner(board)
+  if won?(board) != nil
+    winner = board[won?(board)[0]]
+  end
+end
